9878fcb98fb80e8eba689bbc413021dc87d2e4c2
Yu0614 <azu369yu@gmail.com>
2020-11-22 17:57:04 +0900

    [#3] feat: update eslint packages and rules and add eslintignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,14 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
-  ],
+
   parserOptions: {
     ecmaVersion: 2020
   },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  },
+
   overrides: [
     {
       files: [
@@ -27,5 +19,56 @@ module.exports = {
         mocha: true
       }
     }
-  ]
+  ],
+
+  extends: [
+    "plugin:vue/vue3-essential",
+    "plugin:vue/vue3-strongly-recommended",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    "@vue/prettier/@typescript-eslint",
+  ],
+  plugins: [
+    "vue",
+  ],
+  rules: {
+    // default rules
+    "no-unreachable": "error", 
+    "no-mixed-operators": "error",
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-confusing-arrow": ["error", { "allowParens": false }],
+    "no-tabs": "error",
+    "no-extra-semi": "error",
+
+    // semi colons
+    "semi": ["error", "always"],
+    "semi-style": ["error", "last"],
+    "semi-spacing": ["error", {"after": true, "before": false}],
+
+    // line length
+    "max-len": ["error", {"code": 120, "ignoreUrls": true}],
+
+    // indent
+    "indent": ["error", 4],
+
+    // quotes
+    "quotes": [
+      "error",
+      "single",
+      { "avoidEscape": true, "allowTemplateLiterals": false }
+    ],
+
+    // html
+    "vue/html-self-closing": [
+      "error",
+      {
+        "html": {
+          "void": "any"
+        }
+      }
+    ],
+    "vue/html-closing-bracket-newline": [2, {"multiline": "never"}],
+    "vue/html-indent": "error"
+  },
 };
