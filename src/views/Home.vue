5556f6a40bbed71027f997f8eb8c4337821f08a0
Yu0614 <azu369yu@gmail.com>
2020-11-22 22:38:53 +0900

    [#4] refactor: remove unnecessary components and remove routes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,18 +3,18 @@
     <img
       alt="Vue logo"
       src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '../components/example/HelloWorld.vue'; // @ is an alias to /src
+
 
 export default defineComponent({
     name: 'Home',
     components: {
-        HelloWorld
+      
+        
     }
 });
 </script>
